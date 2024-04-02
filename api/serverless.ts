09891d9a3f897f0bff71ca4ab30aef8255158dce
Ramses <ramses2.blanco@gmail.com>
2024-04-02 14:39:43 -0300

    feat: serverless app

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,7 +1,20 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { app } from "../src/server";
 
+import Fastify from "fastify";
+// Instantiate Fastify with some config
+const serverlessApp = Fastify({
+  logger: false,
+});
+
+
+serverlessApp.get('/', async (req: FastifyRequest, res: FastifyReply) => {
+  res.status(200).send({
+      hello: 'World from serverlessApp'
+  })
+})
+
 export default async (req: FastifyRequest, res: FastifyReply) => {
-  await app.instance.ready()
-  app.instance.server.emit("request", req, res)
+  await serverlessApp.ready()
+  serverlessApp.server.emit("request", req, res)
 }
