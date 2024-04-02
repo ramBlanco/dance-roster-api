1ff180c65f008b4ae15b8fe37f05364e3d4f0230
Ramses <ramses2.blanco@gmail.com>
2024-04-02 15:56:35 -0300

    fix: mannualy plugin

--- build/infrastructure/webserver/server.js
+++ build/infrastructure/webserver/server.js
@@ -3,14 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const autoload_1 = __importDefault(require("@fastify/autoload"));
 const fastify_1 = __importDefault(require("fastify"));
 const morgan_1 = __importDefault(require("morgan"));
 const validation_1 = require("../../domain/validation");
 const config_1 = __importDefault(require("../config/config"));
 const dependencyInjection_1 = require("../config/dependencyInjection");
 const logger_1 = require("../config/logger");
+const _00_genericPlugin_1 = __importDefault(require("../plugins/00-genericPlugin"));
+const _01_swagggerPlugin_1 = __importDefault(require("../plugins/01-swagggerPlugin"));
+const _02_reateLimitPlugin_1 = __importDefault(require("../plugins/02-reateLimitPlugin"));
+const _03_diPlugin_1 = __importDefault(require("../plugins/03-diPlugin"));
+const _04_jwtPlugin_1 = __importDefault(require("../plugins/04-jwtPlugin"));
+const _05_squelizePlugin_1 = __importDefault(require("../plugins/05-squelizePlugin"));
 morgan_1.default.token('remote-addr', (req, _res) => {
     return req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'] : req.socket.remoteAddress;
 });
@@ -22,7 +26,13 @@ class App {
         this.instance = (0, fastify_1.default)({ logger: logger_1.envToLogger[config_1.default.api.logger] });
         // schemas for request
         (0, validation_1.registerSchemas)(this.instance);
-        this.instance.register(autoload_1.default, { dir: (0, path_1.join)(__dirname, '../', 'plugins') });
+        // this.instance.register(autoLoad, { dir: join(__dirname, '../', 'plugins') })
+        this.instance.register(_00_genericPlugin_1.default);
+        this.instance.register(_01_swagggerPlugin_1.default);
+        this.instance.register(_02_reateLimitPlugin_1.default);
+        this.instance.register(_03_diPlugin_1.default);
+        this.instance.register(_04_jwtPlugin_1.default);
+        this.instance.register(_05_squelizePlugin_1.default);
         appConfig.routes.forEach((route) => this.instance.register(route.routes, { prefix: route.prefixRoute }));
     }
     listen() {
--- src/infrastructure/webserver/server.ts
+++ src/infrastructure/webserver/server.ts
@@ -8,6 +8,13 @@ import config from '../config/config'
 import { registerDependencies } from '../config/dependencyInjection'
 import { ENVIRONMENTS, envToLogger } from '../config/logger'
 
+import GenericPlugin from "../plugins/00-genericPlugin";
+import SwagggerPlugin from "../plugins/01-swagggerPlugin";
+import ReateLimitPlugin from "../plugins/02-reateLimitPlugin";
+import DiPlugin from "../plugins/03-diPlugin";
+import JwtPlugin from "../plugins/04-jwtPlugin";
+import SquelizePlugin from "../plugins/05-squelizePlugin";
+
 Morgan.token('remote-addr', (req, _res) => {
   return req.headers['x-forwarded-for'] ? (req.headers['x-forwarded-for'] as string) : req.socket.remoteAddress
 })
@@ -25,7 +32,14 @@ class App {
     // schemas for request
     registerSchemas(this.instance)
 
-    this.instance.register(autoLoad, { dir: join(__dirname, '../', 'plugins') })
+    // this.instance.register(autoLoad, { dir: join(__dirname, '../', 'plugins') })
+
+    this.instance.register(GenericPlugin)
+    this.instance.register(SwagggerPlugin)
+    this.instance.register(ReateLimitPlugin)
+    this.instance.register(DiPlugin)
+    this.instance.register(JwtPlugin)
+    this.instance.register(SquelizePlugin)
 
     appConfig.routes.forEach((route) => this.instance.register(route.routes, { prefix: route.prefixRoute }))
   }
